bc31a61725856ff02d7b10c9e4c79225f5d1a187
lllacoxlll <lllacoxlll@gmail.com>
2023-08-04 02:13:23 -0500

    Add client submodule

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
 import cookieParser from "cookie-parser"
+import cors from "cors"
 // const express = require("express")
 const app = express()
 dotenv.config()
@@ -24,8 +25,8 @@ mongoose.connection.on("disconnected", () => {
 })
 
 //middleware
+app.use(cors())
 app.use(cookieParser())
-
 app.use(express.json())
 
 app.use("/api/auth", authRoute);
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,7 +1,7 @@
 import express from "express";
 //import Hotel from "../models/Hotel.js";
 //import { createError } from "../utils/error.js";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -16,9 +16,11 @@ router.put("/:id", verifyAdmin, updateHotel);
 router.delete("/:id", verifyAdmin, deleteHotel);
 
 //GET
-router.get("/:id", getHotel);
+router.get("/find/:id", getHotel);
 
 //GET ALL
 router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
 
 export default router
